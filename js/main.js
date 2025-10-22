5eb8d23f286fe075d435d03bbd899c74626ea26c
kawthwe <kawther.mohamed.81@gmail.com>
2025-10-22 17:26:16 +0300

    control

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-var productNameInput=document.getElementById('productNameInput')
-var productPriceInput=document.getElementById('productPriceInput')
-var productMatInput=document.getElementById('productMatInput')
-var productShippingInput=document.getElementById('productShippingInput')
-var productSizesInput=document.getElementById('productSizesInput')
-var productDescriptionInput=document.getElementById('productDescriptionInput')
+var productNameInput = document.getElementById('productNameInput');
+var productPriceInput = document.getElementById('productPriceInput');
+var productMatInput = document.getElementById('productMatInput');
+var productShippingInput = document.getElementById('productShippingInput');
+var productSizesInput = document.getElementById('productSizesInput');
+var productDescriptionInput = document.getElementById('productDescriptionInput');
 var productImageInput = document.getElementById('productImageInput');
 
-
-var productsContainer = document.getElementById('productsContainer');
 var productList = JSON.parse(localStorage.getItem('products')) || [];
 
 function addProduct() {
-  // نحول الصورة إلى Base64 علشان نقدر نخزنها في LocalStorage
   var file = productImageInput.files[0];
   if (file) {
     var reader = new FileReader();
     reader.onload = function(e) {
-      saveProduct(e.target.result); // نرسل الصورة بعد التحويل
+      saveProduct(e.target.result);
     };
     reader.readAsDataURL(file);
   } else {
-    saveProduct(''); // في حال مافيش صورة
+    saveProduct('');
   }
 }
 
@@ -35,42 +32,46 @@ function saveProduct(imageData) {
     image: imageData
   };
 
-productList.push(product)
-  localStorage.setItem('products', JSON.stringify(productList)); // نحفظها
+  productList.push(product);
+  localStorage.setItem('products', JSON.stringify(productList));
 
-clearInput()
-displayData()
-console.log(productList);
-alert("Product added successfully!");
-
+  clearInput();
+  displayData();
+  alert("Product added successfully!");
 }
-function clearInput(){
- productNameInput.value=""
- productPriceInput.value=""
- productMatInput.value =''
- productShippingInput.value=''
- productSizesInput.value =''
- productDescriptionInput.value =''
 
+function clearInput() {
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  productMatInput.value = "";
+  productShippingInput.value = "";
+  productSizesInput.value = "";
+  productDescriptionInput.value = "";
+  productImageInput.value = "";
 }
-// function displayData(){
-//     cartona=''
-//        for( var i=0 ;i< productList.length ;i ++){
-//       cartona += `
-//        <div class="col-md-4">
-//         <div class="card w-100 ">
-//           <p>${i}</p>
-//           <h2>* ${productList [i].name} *</h2>
-//           <h3 class="m=auto text-danger">${productList [i].price}</h3>
-//            <button  onClick = "dleateItem(${i})"  class="btn btn-outline-danger"> Delete</button>
-//          </div>
-//      </div>
-//        `
-//     }
-//     document.getElementById('dataCard').innerHTML=cartona
-// }
-function dleateItem(index){
-   productList.splice(index,1)
-   displayData()
 
+function displayData() {
+  var cartona = '';
+  for (var i = 0; i < productList.length; i++) {
+    cartona += `
+      <div class="col-md-4">
+        <div class="card w-100">
+          <img src="${productList[i].image}" class="w-100" alt="Product Image">
+          <h2>* ${productList[i].name} *</h2>
+          <h3 class="text-danger">${productList[i].price}</h3>
+          <button onClick="deleteItem(${i})" class="btn btn-outline-danger">Delete</button>
+          <a href="product-01.html">SHOW MORE</a>
+        </div>
+      </div>
+    `;
+  }
+  document.getElementById('dataCard').innerHTML = cartona;
 }
+
+function deleteItem(index) {
+  productList.splice(index, 1);
+  localStorage.setItem('products', JSON.stringify(productList));
+  displayData();
+}
+
+displayData();
